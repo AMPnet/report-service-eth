f84629dff0229873d4cfda85c895fe017e785363
Eugen Druzin <edruzin@gmail.com>
2021-09-14 16:23:28 +0200

    Change rpc for mumbai

--- src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
+++ src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
@@ -3,7 +3,7 @@ package com.ampnet.reportserviceeth.blockchain.properties
 @Suppress("MagicNumber")
 enum class Chain(val id: Long, val rpcUrl: String, val infura: String?) {
     MATIC_MAIN(137, "https://polygon-rpc.com/", null),
-    MATIC_TESTNET_MUMBAI(80001, "https://rpc-mumbai.matic.today/", null),
+    MATIC_TESTNET_MUMBAI(80001, "https://matic-mumbai.chainstacklabs.com/", null),
     ETHEREUM_MAIN(1, "https://cloudflare-eth.com/", "https://mainnet.infura.io/v3/"),
     HARDHAT_TESTNET(31337, "http://hardhat:8545", null);
 
