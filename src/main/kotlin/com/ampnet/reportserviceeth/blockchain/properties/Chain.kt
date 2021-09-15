1e493788f1697ce43f0d6ae6939affcc78ad6b6d
Eugen Druzin <githinho@users.noreply.github.com>
2021-09-15 08:06:03 +0000

    Remove infura matic (#26)
    * Remove infura for matic chains
    
    * Change rpc for mumbai

--- src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
+++ src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
@@ -2,8 +2,8 @@ package com.ampnet.reportserviceeth.blockchain.properties
 
 @Suppress("MagicNumber")
 enum class Chain(val id: Long, val rpcUrl: String, val infura: String?) {
-    MATIC_MAIN(137, "https://rpc-mainnet.matic.network/", "https://polygon-mainnet.infura.io/v3/"),
-    MATIC_TESTNET_MUMBAI(80001, "https://rpc-mumbai.matic.today/", "https://polygon-mumbai.infura.io/v3/"),
+    MATIC_MAIN(137, "https://polygon-rpc.com/", null),
+    MATIC_TESTNET_MUMBAI(80001, "https://matic-mumbai.chainstacklabs.com/", null),
     ETHEREUM_MAIN(1, "https://cloudflare-eth.com/", "https://mainnet.infura.io/v3/"),
     HARDHAT_TESTNET(31337, "http://hardhat:8545", null);
 
