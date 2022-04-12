2289d6d3939c456baad04236cef5a1cd1cea3f34
Eugen Druzin <edruzin@gmail.com>
2022-04-12 12:03:03 +0200

    Change poa properties

--- src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
+++ src/main/kotlin/com/ampnet/reportserviceeth/blockchain/properties/Chain.kt
@@ -9,7 +9,7 @@ enum class Chain(val id: ChainId, val rpcUrl: String, val infura: String?) {
     ETHEREUM_MAIN(ChainId(1L), "https://cloudflare-eth.com/", "https://mainnet.infura.io/v3/"),
     GOERLI_TESTNET(ChainId(5L), "https://goerli.prylabs.net/", "https://goerli.infura.io/v3/"),
     HARDHAT_TESTNET(ChainId(31337L), "http://hardhat:8545", null),
-    AMPNET_POA(ChainId(1337L), "https://poa.ampnet.io/rpc", null);
+    AMPNET_POA(ChainId(1984L), "https://poa.ampnet.io/rpc", null);
 
     companion object {
         private val map = values().associateBy(Chain::id)
